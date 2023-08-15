085845b06d1cd58b9def70afcf721eb49bb9951d
the-casle <redeyedtreefrog28@gmail.com>
2023-08-15 12:41:37 -0500

    Adding start of wrapper

---
+++ volume_adjustment/__init__.py
@@ -0,0 +1,8 @@
+# Multistrand nucleic acid kinetic simulator
+# Copyright (c) 2008-2023 California Institute of Technology. All rights reserved.
+# The Multistrand Team (help@multistrand.org)
+
+from .nupack_wrapper import *
+
+def version():
+    return '0.1'
